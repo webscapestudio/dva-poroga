29a750417b54f62a07aa8e632d4ac3a81f78826a
John Doe <johndoe@example.com>
2024-06-21 13:24:07 +0300

    feature: блог + карточка блога + адаптивы

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,9 +1,22 @@
-import Swiper from 'swiper'
-
 export const sliders = () => {
   // eslint-disable-next-line no-new
-  new Swiper('.mySwiper', {
-    slidesPerView: 2,
+  new Swiper('.blog-section-slider', {
+    spaceBetween: 16,
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1
+      },
+      630: {
+        slidesPerView: 2
+      },
+      1024: {
+        slidesPerView: 3
+      },
+      1440: {
+        slidesPerView: 3
+      }
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev'
