9a6248ebf975bb3246d793a0bb7f6299b70d9920
John Doe <johndoe@example.com>
2024-06-28 18:37:54 +0300

    asd

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
+// import micromodal from 'micromodal'
+import micromodal from 'micromodal'
 import { accordition } from './modules/accordition'
 import { sliders } from './modules/sliders'
 
 accordition()
 sliders()
+
+micromodal.init()
